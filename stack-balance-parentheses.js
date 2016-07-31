cd9d373b2f7bc21a58aec43767f71d1754ad3093
Maulik Suchak <suchak.maulik179@gmail.com>
2016-07-31 11:20:22 -0700

    Update stack-balance-parentheses.js

--- stack-balance-parentheses.js
+++ stack-balance-parentheses.js
@@ -1,25 +1,31 @@
-var str = '([{}][{)}])';
-var stack = [];
+console.log('([][]){({}})}');
+console.log(isBalanced('([][]){({}})}'));
 
-console.log(str);
-console.log(isBalanced(str));
+console.log('([][])({}})}');
+console.log(isBalanced('([][])({}})}'));
+
+console.log('([][]{})');
+console.log(isBalanced('([][]{})'));
 
 function isBalanced (str) {
+  var stack = [];
   for (var i=0; i<str.length; i++) {
     if (str[i] === "{" || str[i] === "(" || str[i] === "[") {
       stack.push(str[i]);
     } else {
       if (i !== 0){
-        var element = stack[stack.length-1];      
+        var element = stack[stack.length-1];
         if ((str[i] === ")" && element === '(')|| 
             (str[i] === "]" && element === '[')|| 
             (str[i] === "}" && element === '{')) {
             stack.pop();
+        } else {
+          return false;
         }
       }
     } 
   }
-  if (stack.length < 1) {
+  if (stack.length === 0) {
     return true;
   } else {
     return false;
